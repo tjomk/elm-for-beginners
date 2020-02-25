97bda0bd7f66d81069a2d170709d29838a8c005a
tjomk <artjom@protonmail.com>
2020-02-25 17:05:52 +0200

    Add comments to the code

--- syntax/src/Main.elm
+++ syntax/src/Main.elm
@@ -7,7 +7,10 @@ import Html.Events exposing (onClick)
 
 
 
--- this is our main function
+-- This is our main function. It describes our application
+-- and what gets rendered on the screen. We use `Browser.sanbox`
+-- function since we don't need any event listeners or ajax
+-- requests.
 
 
 main =
@@ -15,8 +18,12 @@ main =
 
 
 
-{- This is our model
-   that has only one attribute
+{- This is our model, it describes which data we are going to
+   store and use. For this example we just count clicks. The
+   use of the record type here is intentional for learning
+   experience, however for a single attribute like `clicks`
+   we could have simplified the model to just `Int` type:
+   `type alias Model = Int`.
 -}
 
 
@@ -25,8 +32,24 @@ type alias Model =
     }
 
 
+
+-- This is the message the our view code will send to the
+-- update function whenever some event happens. We want to
+-- track a single event only, however it is common to have
+-- lots of different types here, e.g. form submit, data
+-- received, and so on.
+
+
 type Msg
     = ClickButton
+
+
+
+{- The init function that we pass to the `Browser.sandbox`.
+   This initializes the state of the application, i.e. the
+   first time our code gets rendered on the screen, what
+   values are we going to use/show the user.
+-}
 
 
 init : Model
@@ -34,11 +57,26 @@ init =
     Model 0
 
 
+
+{- Update function is used to update our model. It is triggered
+   by various events that we handle here and update data
+   accordingly. Some of these events can be button clicks or
+   key presses, others can be API responses or scheduled events.
+-}
+
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         ClickButton ->
             { model | clicks = model.clicks + 1 }
+
+
+
+{- Finally the view function describes the UI that we see on the
+   screen. It receives a model and outputs HTML. Button
+   element fires the `ClickButton` message when the user clicks it.
+-}
 
 
 view : Model -> Html Msg
